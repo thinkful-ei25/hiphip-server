da5091cff6a614e4c209ddc4e9d88a3fbbc07de0
Sam Dalton <spd4nf@virginia.edu>
2019-01-15 14:51:46 -0600

    took away aisles

--- models/store.js
+++ models/store.js
@@ -5,12 +5,6 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
   name: { type: String, required: true },
   address: { type: String, required: true, unique: true },
-  aisles: [
-    {
-      aisleNo: { type: String },
-      categories: { type: Array },
-    },
-  ],
 });
 
 schema.set('toJSON', {
