a0a38ec223be202f935840046c11b3d70fd3a249
Arun Seehra <seehra.a@gmail.com>
2019-01-22 14:36:06 -0600

    Fix 500 error when saving a missing aisleLocation
    This would prevent checking off an item that did not have aisleLocation
    information embedded in it. Previously, POST would not create an
    aisleLocation for every item. Thus, old item entries would cause the server to
    throw a typeerror. Now, old items will be updated to have aisle information.
    
    Additionally, we now validate against the situation where a user attempts to
    enter aisle information for a list that is not attached to a store (we throw a
    HTTP 422 error).

--- routes/items.js
+++ routes/items.js
@@ -94,32 +94,57 @@ router
     if (!mongoose.Types.ObjectId.isValid(listId)) {
       throw new HttpError(422, `${listId} is not a valid ObjectId`);
     }
+    let list;
+    let item;
     ShoppingList.findById(listId)
       .populate('items.aisleLocation')
-      .then(list => {
+      .then(_list => {
+        list = _list;
         if (!list) {
           throw new NotFoundError();
         }
         const { name, isChecked, aisleLocation } = req.body;
-        const item = list.items.id(id);
+        item = list.items.id(id);
         if (!item) {
           throw new NotFoundError();
         }
+
+        if (aisleLocation && !list.store) {
+          throw new HttpError(
+            422,
+            'Cannot update aisle information for a list not associated with a store'
+          );
+        }
+
         if (name) {
           item.name = name;
         }
+
         if (isChecked !== item.isChecked) {
           item.isChecked = isChecked;
         }
-        if (aisleLocation) {
-          item.aisleLocation.aisleNo = aisleLocation;
-        }
 
-        return Promise.all([list.save(), item.aisleLocation.save()]);
+        /* eslint-disable indent */
+        return Promise.resolve(
+          list.store
+            ? getCategory(item.name).then(category =>
+                findAndUpdateAisleLocation(
+                  list.store._id,
+                  category._id,
+                  aisleLocation
+                )
+              )
+            : null
+        );
+        /* eslint-enable indent */
       })
-      .then(([list]) => {
-        const item = list.items.id(id);
-        res.json({ item });
+      .then(_aisleLocation => {
+        item.aisleLocation = _aisleLocation;
+        return list.save();
+      })
+      .then(_list => {
+        const _item = _list.items.id(id);
+        res.json({ item: _item });
       })
       .catch(next);
   })
