bd75ac91ab0e4bb2b7e801876299eb9b63f0ee8a
Sam Dalton <spd4nf@virginia.edu>
2019-01-24 14:50:01 -0600

    added validation to api routes

--- routes/yelp.js
+++ routes/yelp.js
@@ -4,11 +4,17 @@ const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 
 const { YELP_AUTH_TOKEN } = require('../config');
+const { ValidationError } = require('../errors');
 
 const router = express.Router();
 
 router.route('/coords').get((req, res, next) => {
   const { term, category, latitude, longitude } = req.query;
+  const requiredFields = ['term', 'category', 'latitude', 'longitude'];
+  const missingField = requiredFields.find(field => req.query[field] === '');
+  if (missingField) {
+    throw new ValidationError(missingField, 'Missing field', 422);
+  }
   fetch(
     `https://api.yelp.com/v3/businesses/search?term=${term}&category=${category}&latitude=${latitude}&longitude=${longitude}`,
     {
@@ -27,6 +33,11 @@ router.route('/coords').get((req, res, next) => {
 
 router.route('/location').get((req, res, next) => {
   const { term, category, location } = req.query;
+  const requiredFields = ['term', 'category', 'location'];
+  const missingField = requiredFields.find(field => req.query[field] === '');
+  if (missingField) {
+    throw new ValidationError(missingField, 'Missing field', 422);
+  }
   fetch(
     `https://api.yelp.com/v3/businesses/search?term=${term}&category=${category}&location=${location}`,
     {
