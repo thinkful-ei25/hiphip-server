0efece78a20309117d847dbf9f03e7f9c652fa5f
Sam Dalton <spd4nf@virginia.edu>
2019-01-23 22:15:45 -0600

    changed yelp routes to reflect either coordinates or location

--- routes/yelp.js
+++ routes/yelp.js
@@ -8,10 +8,27 @@ const { ValidationError } = require('../errors');
 const router = express.Router();
 
 const jsonParser = bodyParser.json();
-router.route('/').get((req, res, next) => {
+router.route('/coords').get((req, res, next) => {
   const { term, category, latitude, longitude } = req.query;
   fetch(
     `https://api.yelp.com/v3/businesses/search?term=${term}&category=${category}&latitude=${latitude}&longitude=${longitude}`,
+    {
+      method: 'get',
+      headers: {
+        Authorization: `Bearer FiidsBWee5-Rbjey4BkRf3lTwhfVXtIeqBaoxx_aPo1-CzsBorzm2jeNI9CUoKwW4asFAlWhysLTP5UEMmLm5QR3L1VZRmmweJv2A7YQB8WAZCeQ8_ckNwdbWCpCXHYx`,
+      },
+    }
+  )
+    .then(res => res.json())
+    .then(businesses => {
+      res.json(businesses);
+    });
+});
+
+router.route('/location').get((req, res, next) => {
+  const { term, category, location } = req.query;
+  fetch(
+    `https://api.yelp.com/v3/businesses/search?term=${term}&category=${category}&location=${location}`,
     {
       method: 'get',
       headers: {
