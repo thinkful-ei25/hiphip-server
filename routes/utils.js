3f4e2db6f1ffe6db41b88eb0172d412005668b23
mischb <mischarberlin@gmail.com>
2019-01-26 12:01:30 -0500

    fixed deletion

--- routes/utils.js
+++ routes/utils.js
@@ -49,42 +49,27 @@ function moveItem(items, itemId, head, down = false) {
   newItems[prevPrevItemIndex].next = newItems[prevItemIndex].next;
   newItems[prevItemIndex].next = newItems[itemIndex].next;
   newItems[itemIndex].next = newNext;
+
   return { newItems, head };
 }
 
 //for every item after the item i'm deleting add one to next (except the last one)
-function removeItem(items, head, deletedIndex) {
-  const manSortItems = [];
-  let currentIndex = head;
+function sortItems(items, head) {
+  const sorted = [];
+  let i = head;
   //for each one, push it in, if index is equal to skip, if greater than, subtract one from next
-  items.forEach((item, index) => {
-    if (index < deletedIndex) {
-      manSortItems.push(item);
-    } else if (index === items.length) {
-      manSortItems.push(item);
-    } else if (index > deletedIndex) {
-      item.next -= 1;
-      manSortItems.push(item);
+  while (items[i]) {
+    sorted.push(items[i]);
+    i = items[i].next;
+    if (!i) {
+      sorted[sorted.length - 1].next = null;
+    } else {
+      sorted[sorted.length - 1].next = sorted.length;
     }
-  });
-
-  while (items[currentIndex]) {
-    manSortItems.push(items[currentIndex]);
-    currentIndex = items[currentIndex].next;
   }
-  return manSortItems;
+  return sorted;
 }
-function isEmpty(items, head, deletedIndex) {
-  let empty = true;
-  items.forEach(item => {
-    if (items[head] === item && head === deletedIndex) {
-      return;
-    } else if (item.next) {
-      empty = false;
-    }
-  });
-  return empty;
-}
+
 function deleteItem(items, itemId, head) {
   let itemIndex;
   items.forEach((item, i) => {
@@ -97,20 +82,15 @@ function deleteItem(items, itemId, head) {
     head = -1;
     return { newItems, head };
   }
-  console.log(items);
-  console.log(itemIndex, head);
-  if (isEmpty(items)) {
-    head = -1;
-  } else if (itemIndex === head) {
+  if (itemIndex === head) {
     head = items[itemIndex].next;
   } else {
     let prevItemIndex = findPrevItemIndex(items, itemIndex);
     items[prevItemIndex].next = items[itemIndex].next;
   }
 
-  // const newItems = removeItem(items, head);
-  const newItems = items;
-  console.log(newItems, head);
+  const newItems = sortItems(items, head);
+  head = 0;
   return { newItems, head, itemIndex };
 }
 module.exports = { moveItem, deleteItem };
